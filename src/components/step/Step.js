58238801e279d4c4bb10ecc90b4a04e0f8b8d717
Leonardo Omaña <leotangram@gmail.com>
2020-07-25 12:15:51 -0500

    Add step styles

--- src/components/step/Step.js
+++ src/components/step/Step.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Step.scss'
 
-const Step = props => {
-  return <div>Step</div>
+const Step = ({ id, step, value }) => {
+  return (
+    <div
+      className={id === 'pig' ? 'step step--principal' : 'step'}
+      style={{ width: `${value}%` }}
+    >
+      {step}
+    </div>
+  )
 }
 
-Step.propTypes = {}
+Step.propTypes = {
+  id: PropTypes.string.isRequired,
+  step: PropTypes.element.isRequired,
+  value: PropTypes.number.isRequired
+}
 
 export default Step
