94bf1c9b7d2075d0df05502f4f64e1c056ef2fe6
Leonardo Omaña <leotangram@gmail.com>
2020-07-25 12:15:32 -0500

    Add steps

--- src/components/progress-bar/ProgressBar.js
+++ src/components/progress-bar/ProgressBar.js
@@ -1,14 +1,50 @@
 import React from 'react'
 import './ProgressBar.scss'
+import PigIcon from '../../assets/icons/PigIcon'
+import MapIconDisabled from '../../assets/icons/MapIconDisabled'
+import MapIconEnabled from '../../assets/icons/MapIconEnabled'
+import SandIconDisabled from '../../assets/icons/SandIconDisabled'
+import SandIconEnabled from '../../assets/icons/SandIconEnabled'
+import GreatestTreasureIconDisabled from '../../assets/icons/GreatestTreasureIconDisabled'
+import GreatestTreasureIconEnabled from '../../assets/icons/GreatestTreasureIconEnabled'
 import Step from '../step/Step'
 
 const ProgressBar = () => {
+  const steps = [
+    {
+      id: 'pig',
+      step: <PigIcon size={35} />,
+      value: 0
+    },
+    {
+      id: 'firstStep',
+      step: '' ? <MapIconDisabled size={35} /> : <MapIconEnabled size={35} />,
+      value: 25
+    },
+    {
+      id: 'secondStep',
+      step: '' ? <SandIconDisabled size={35} /> : <SandIconEnabled size={35} />,
+      value: 50
+    },
+    {
+      id: 'thirdStep',
+      step: '' ? (
+        <GreatestTreasureIconDisabled size={35} />
+      ) : (
+        <GreatestTreasureIconEnabled size={35} />
+      ),
+      value: 75
+    }
+  ]
+
   return (
     <div className="progress-bar">
       <div className="progress-bar__circle" />
       <div className="progress-bar__bar-section">
         <div className="progress-bar__bar-section__bar" />
-        <Step />
+        {steps.map(step => (
+          <Step key={step.id} {...step} />
+        ))}
       </div>
     </div>
   )
